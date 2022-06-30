ed8023d00eab83c929902f3c086970e43bb02506
Enrique Morato <quiquemorato@gmail.com>
2022-06-30 20:44:23 +0200

    Changes on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <h1 className='font-black text-5xl text-center md:w-1/2 mx-auto'>
-      Patients track {''}
+      Patients track {''} with Github {''}
       <span className='text-indigo-600'>Veterinarian</span>
     </h1>
   )
